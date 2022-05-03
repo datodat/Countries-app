76dc25fb242d1a52b8922fdb53763256ad365883
Dato Kevlishvili <dato.kevlishvili.1990@gmail.com>
2022-05-04 01:55:29 +0400

    countries main page

--- src/App.js
+++ src/App.js
@@ -11,17 +11,63 @@ const colors = ['#eeeeee', '#ffffff', '#3b3f52', '#272a39'];
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
   const [countries, setCountries] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  const [region, setRegion] = useState(null);
 
   useEffect(() => {
+    handleAll();
+  }, [])
+
+  useEffect(() => {
+    if(region){
+      handleRegion();
+    }else{
+      handleAll();
+    }
+  }, [region])
+
+  const handleAll = () => {
     axios.get('https://restcountries.com/v2/all')
+      .then(res => {
+        setSearchWord('');
+        setCountries(res.data);
+      })
+      .catch(error => console.log(error.message))
+  }
+
+  const handleSearch = () => {
+    if(searchWord){
+      axios.get(`https://restcountries.com/v2/name/${searchWord}`)
+        .then(res => {
+          setCountries(res.data);
+          setSearchWord('');
+        })
+        .catch(error => console.log(error.message)) 
+    }else{
+      handleAll();
+    }
+  }
+
+  const handleRegion = () => {
+    axios.get(`https://restcountries.com/v2/continent/${region}`)
       .then(res => setCountries(res.data))
       .catch(error => console.log(error.message))
-  }, [])
+  }
+
+  const handleInfo = (name) => {
+    if(name){
+      axios.get(`https://restcountries.com/v2/name/${name}`)
+        .then(res => setCountries(res.data))
+        .catch(error => console.log(error.message))
+    }else{
+      return;
+    }
+  }
 
   return (
     <div className='container' style={{ backgroundColor: darkMode ? colors[3] : colors[1], color: darkMode ? 'white' : 'black' }}>
       <header className='header' style={{ backgroundColor: darkMode ? colors[2] : colors[0] }}>
-        <h1>Where in the world?</h1>
+        <h1 onClick={handleAll}>Where in the world?</h1>
         <button onClick={() => setDarkMode(!darkMode)}>
           <i className="fa-solid fa-moon icon-dark"></i>
           <i className="fa-solid fa-brightness icon-light"></i>
@@ -31,11 +77,23 @@ const App = () => {
       <div className='body-container'>
         <div className='search-div'>
           <div className='input-div'>
-            <i className="fa-solid fa-magnifying-glass"></i>
-            <input type='text' placeholder='Search for a country...' />
+            <i 
+              className="fa-solid fa-magnifying-glass"
+              onClick={handleSearch}
+            ></i>
+            <input 
+              type='text' 
+              placeholder='Search for a country...'
+              value={searchWord}
+              onChange={({target}) => setSearchWord(target.value)}
+            />
           </div>
           <div className='dropdown-div'>
-            <input list="browsers" placeholder='Filter by Region:' />
+            <input 
+              list="browsers" 
+              placeholder='Filter by Region:'
+              onChange={({target}) => setRegion(target.value)}  
+            />
             <datalist id="browsers">
               <option value="Africa" />
               <option value="America" />
@@ -49,7 +107,7 @@ const App = () => {
         <div className='countries-div'>
           {countries.length > 0 && countries.map((i, index) => {
             return (
-              <Card key={index} data={i} />
+              <Card key={index} data={i} handleInfo={handleInfo} />
             )
           })}
         </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const Card = ({ data }) => {
+const Card = ({ data, handleInfo }) => {
   return (
-    <div className='card'>
+    <div className='card' onClick={() => handleInfo(data.name)}>
       <div className='card-top'>
         <img src={data.flags.png} alt='logo' />
       </div>
       <div className='card-bottom'>
-        {data.name}
+        <h4>{data.name}</h4>
+        <p className='card-population'>Population: {data.population}</p>
+        <p className='card-region'>Region: {data.region}</p>
+        <p className='card-capital'>Capital: {data.capital}</p>
       </div>
     </div>
   );
